57edcd21a7d7bd3f17850a3ec850cecddb4107c5
AlexShukhman <alex.jacob.shukhman@gmail.com>
2017-01-30 13:36:36 -0500

    did a bunch of api work

--- api.js
+++ api.js
@@ -25,13 +25,112 @@ app.use(morgan('dev'));
 // =======================
 // routes ================
 // =======================
-// basic route
-app.get('/', function (req, res) {
-	res.send('Hello! The API is at http://localhost:' + port + '/api');
+// create user
+app.get('/setup', function (req, res) {
+	// create a sample user
+	var nick = new User({
+		name: req.body.name
+		, password: req.body.password
+		, admin: req.body.admin
+	});
+	// save the sample user
+	nick.save(function (err) {
+		if (err) throw err;
+		console.log('User saved successfully');
+		res.json({
+			success: true
+		});
+	});
 });
 // API ROUTES -------------------
-// we'll get to these in a second
+// apiRoutes are routes to helper functions /only/ do not call apiRoutes functions from outside the API
+var apiRoutes = express.Router();
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+// TODO: route middleware to verify a token
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function (req, res) {
+	res.json({
+		message: 'Welcome to the smop. API.'
+	});
+});
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function (req, res) {
+	User.find({}, function (err, users) {
+		res.json(users);
+	});
+});
 // =======================
+// authentication ========
+// =======================
+// authentication script
+apiRoutes.post('/authenticate', function (req, res) {
+	// find the user
+	User.findOne({
+		name: req.body.name
+	}, function (err, user) {
+		if (err) throw err;
+		if (!user) {
+			res.json({
+				success: false
+				, message: 'Authentication failed. User not found.'
+			});
+		}
+		else if (user) {
+			// check if password matches
+			if (user.password != req.body.password) {
+				res.json({
+					success: false
+					, message: 'Authentication failed. Wrong password.'
+				});
+			}
+			else {
+				// if user is found and password is right
+				// create a token
+				var token = jwt.sign(user, app.get('superSecret'), {
+					expiresIn: 1440 * 60 // expires in 24 hours
+				});
+				// return the information including token as JSON
+				res.json({
+					success: true
+					, message: 'Enjoy your token!'
+					, token: token
+				});
+			}
+		}
+	});
+});
+// route middleware to verify a token
+apiRoutes.use(function (req, res, next) {
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	// decode token
+	if (token) {
+		// verifies secret and checks exp
+		jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+			if (err) {
+				return res.json({
+					success: false
+					, message: 'Failed to authenticate token.'
+				});
+			}
+			else {
+				// if everything is good, save to request for use in other routes
+				req.decoded = decoded;
+				next();
+			}
+		});
+	}
+	else {
+		// if there is no token
+		// return an error
+		return res.status(403).send({
+			success: false
+			, message: 'No token provided.'
+		});
+	}
+});
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
 // start the server ======
 // =======================
 app.listen(port);
