a7710217d442e8a148e1d3b128c0855ddd6524ec
AlexShukhman <alex.jacob.shukhman@gmail.com>
2017-01-29 19:05:44 -0500

    set up server

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# smopapi
+#smop. API
+Welcome to the API! Below is some info on what I plan on using:
+
+##Lang/lib:
+* html
+* css
+* js
+* node.js
+* express (no pug, but see below on how to make a page)
+* bootstrap
+* jquery
+* angular.js --> maybe
+
+##Some Helpful Resources 
+* http://justbuildsomething.com/node-js-best-practices/#3
+* https://zellwk.com/blog/crud-express-mongodb/
+
+##html/Pug Etiquette (aka how to make a page)
+* html and Pug files are located in the views folder
+* html files are included in Jade files
+* Pug files are processed into client facing files (eg - if I wanted a page called user.html, it would be called user.pug in the views folder and have 2+ lines of code, all of which are includes to html files *see index.pug for example*
+* **header.html *must* be included in all Pug files**
+* **only html files may be included in Pug files**
+* Each page's html file must close the header before beginning a body
+* headers should not include anything that can be seen by the user
+
+##JS Etiquette
+* Since express is awesome, we probably don't have to write much node stuff for now
+* **no javascripts in html documents,** *they belong in .js documents in the /public/javascripts folder and are to be linked via* \<script src="/public/javascripts/mysource.js">\</script>
--- api.js
+++ api.js
@@ -1,33 +1,38 @@
+// =======================
+// get the packages we need ============
+// =======================
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var api = express();
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
 var User = require('./api/models/user'); // get our mongoose model
 // =======================
 // configuration =========
 // =======================
-console.log('config api');
-mongoose.connect(config.database); // connect to database
-api.set('superSecret', config.secret); // secret variable
+var port = process.env.PORT || 3001; // used to create, sign, and verify tokens
+mongoose.connect(config.database_dev); // connect to database
+app.set('superSecret', config.secret); // secret variable
 // use body parser so we can get info from POST and/or URL parameters
-api.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
 	extended: false
 }));
-api.use(bodyParser.json());
+app.use(bodyParser.json());
 // use morgan to log requests to the console
-api.use(morgan('dev'));
+app.use(morgan('dev'));
 // =======================
 // routes ================
 // =======================
 // basic route
-console.log('configged, now set up routes');
-api.get('/', function (req, res) {
-	res.send('Hello! The API is at http://localhost:' + apiport + '/api');
+app.get('/', function (req, res) {
+	res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 // API ROUTES -------------------
 // we'll get to these in a second
-// note: no view engine setup or favicon setup
-module.exports = api;
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
