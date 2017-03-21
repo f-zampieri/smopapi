4ab75926735db9bd4bcaaa8f7ebffe1fa412d5b5
alexshukhman <alexjacobshukhman@gmail.com>
2017-03-21 16:54:02 -0700

    adding a lot of new features, sorry mai

--- api.js
+++ api.js
@@ -65,7 +65,85 @@ apiRoutes.get('/', function (req, res) {
 		message: 'Welcome to the smop. API.'
 	});
 });
+// =======================
+// authentication ========
+// =======================
+// authentication script
+apiRoutes.post('/authenticate', function (req, res) {
+	// find the user
+	User.findOne({
+		name: req.body.name
+	}, function (err, user) {
+		if (err) throw err;
+		if (!user) {
+			res.json({
+				success: false
+				, message: 'Authentication failed. User not found.'
+			});
+		}
+		else if (user) {
+			// check if password matches
+			var hash = crypto.createHmac('sha512', user.salt);
+			hash.update(req.body.password);
+			var value = hash.digest('hex');
+			if (user.password != value) {
+				res.json({
+					success: false
+					, message: 'Authentication failed. Wrong password.'
+				});
+			}
+			else {
+				// if user is found and password is right
+				// create a token
+				var token = jwt.sign(user, app.get('superSecret'), {
+					expiresIn: 1440 * 60 // expires in 24 hours
+				});
+				// return the information including token as JSON
+				res.json({
+					success: true
+					, message: 'Enjoy your token!'
+					, token: token
+				});
+			}
+		}
+	});
+});
+// route middleware to verify a token
+apiRoutes.use(function (req, res, next) {
+	console.log('running middleware');
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	// decode token
+	if (token) {
+		// verifies secret and checks exp
+		jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+			if (err) {
+				console.log('jwt not authenticated');
+				return res.json({
+					success: false
+					, message: 'Failed to authenticate token.'
+				});
+			}
+			else {
+				// if everything is good, save to request for use in other routes
+				console.log(jwt);
+				req.decoded = decoded;
+				next();
+			}
+		});
+	}
+	else {
+		// if there is no token
+		// return an error
+		console.log('jwt not provided');
+		return res.status(403).send({
+			success: false
+			, message: 'No token provided.'
+		});
+	}
+});
 apiRoutes.get('/checkToken', function (req, res) {
+	console.log('running checkToken');
 	res.json({
 		success: true
 	});
@@ -132,79 +210,6 @@ apiRoutes.get('/get_info', function (req, res) {
 			}
 		}
 	});
-});
-// =======================
-// authentication ========
-// =======================
-// authentication script
-apiRoutes.post('/authenticate', function (req, res) {
-	// find the user
-	User.findOne({
-		name: req.body.name
-	}, function (err, user) {
-		if (err) throw err;
-		if (!user) {
-			res.json({
-				success: false
-				, message: 'Authentication failed. User not found.'
-			});
-		}
-		else if (user) {
-			// check if password matches
-			var hash = crypto.createHmac('sha512', user.salt);
-			hash.update(req.body.password);
-			var value = hash.digest('hex');
-			if (user.password != value) {
-				res.json({
-					success: false
-					, message: 'Authentication failed. Wrong password.'
-				});
-			}
-			else {
-				// if user is found and password is right
-				// create a token
-				var token = jwt.sign(user, app.get('superSecret'), {
-					expiresIn: 1440 * 60 // expires in 24 hours
-				});
-				// return the information including token as JSON
-				res.json({
-					success: true
-					, message: 'Enjoy your token!'
-					, token: token
-				});
-			}
-		}
-	});
-});
-// route middleware to verify a token
-apiRoutes.use(function (req, res, next) {
-	// check header or url parameters or post parameters for token
-	var token = req.body.token || req.query.token || req.headers['x-access-token'];
-	// decode token
-	if (token) {
-		// verifies secret and checks exp
-		jwt.verify(token, app.get('superSecret'), function (err, decoded) {
-			if (err) {
-				return res.json({
-					success: false
-					, message: 'Failed to authenticate token.'
-				});
-			}
-			else {
-				// if everything is good, save to request for use in other routes
-				req.decoded = decoded;
-				next();
-			}
-		});
-	}
-	else {
-		// if there is no token
-		// return an error
-		return res.status(403).send({
-			success: false
-			, message: 'No token provided.'
-		});
-	}
 });
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
